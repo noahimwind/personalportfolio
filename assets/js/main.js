7f2ce7294e740aed7ffc1b527e64427ed5a803c3
noahimwind <ninadurkes@gmail.com>
2023-07-28 16:33:59 +0200

    scroll up button appears when scrolling down

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,3 +109,11 @@ function scrollHeader(){
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/* show scroll up */
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
